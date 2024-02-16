0bc7089ad028ad1dd0c52bd00759f9e4d06546f3
phuongluuho <65580209+phuongluuho@users.noreply.github.com>
2024-02-16 22:43:22 +0700

    Update script-appgw.sh

--- script-appgw.sh
+++ script-appgw.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sleep 20s
-apt-get update
-apt-get install stress -y
-apt-get install apache2 -y
-ufw allow 'Apache Full'
-ufw allow 'OpenSSH'
-ufw enable 
-sudo systemctl start apache2
-sudo systemctl enable apache2
+sudo apt-get update
+sudo apt-get install stress -y
+sudo apt-get install apache2 -y
+sudo ufw allow 'Apache Full'
+sudo ufw allow 'OpenSSH'
+sudo ufw enable 
+sudo -i
 cd /var/www/html
 echo "<hmtl><body><h1>Web Server Azure Application Gateway LB </h1></body></html>" > index.html
